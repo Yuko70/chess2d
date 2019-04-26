f45744a7a85192c4d56be384b535f5df72d6d02e
Yuko70 <martin.masek10@gmail.com>
2019-04-26 15:17:54 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
+
+
+let imageAdress = 'https://raw.githubusercontent.com/yuko70/chess2d/master/img/';
+
 class App extends Component {
   constructor() {
     super();
