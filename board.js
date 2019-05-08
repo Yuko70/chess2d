1c09ca4473fb9b771ba05063f3d893933836bf81
Yuko70 <martin.masek10@gmail.com>
2019-05-08 17:01:21 +0200

    2 files modified

--- board.js
+++ board.js
@@ -2,15 +2,28 @@ import Figure from './figure';
 
 export default class Board {
   constructor() {
+    // this.arr = [
+    //   [new Figure('v', 1), new Figure('k', 1), new Figure('s', 1), new Figure('Q', 1), new Figure('K', 1), new Figure('s', 1), new Figure('k', 1), new Figure('v', 1)],
+    //   [new Figure('p', 1), new Figure('p', 1), new Figure('p', 1), new Figure('p', 1), new Figure('p', 1), new Figure('p', 1), new Figure('p', 1), new Figure('p', 1)],
+    //   [null, null, null, null, null, null, null, null],
+    //   [null, null, null, null, null, null, null, null],
+    //   [null, new Figure('p', 1), null, null, null, null, null, null],
+    //   [new Figure('p', 0), null, null, null, null, null, null, null],
+    //   [new Figure('p', 0), new Figure('p', 0), new Figure('p', 0), new Figure('p', 0), new Figure('p', 0), new Figure('p', 0), new Figure('p', 0), new Figure('p', 0)],
+    //   [new Figure('v', 0), new Figure('k', 0), new Figure('s', 0), new Figure('Q', 0), new Figure('K', 0), new Figure('s', 0), new Figure('k', 0), new Figure('v', 0)]
+    // ];
+
     this.arr = [
       [new Figure('v', 1), new Figure('k', 1), new Figure('s', 1), new Figure('Q', 1), new Figure('K', 1), new Figure('s', 1), new Figure('k', 1), new Figure('v', 1)],
       [new Figure('p', 1), new Figure('p', 1), new Figure('p', 1), new Figure('p', 1), new Figure('p', 1), new Figure('p', 1), new Figure('p', 1), new Figure('p', 1)],
       [null, null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null, null],
       [null, new Figure('p', 1), null, null, null, null, null, null],
-      [new Figure('p', 0), null, null, null, null, null, null, null],
+      [new Figure('p', 0), null, null, new Figure('p', 1), null, null, null, null],
       [new Figure('p', 0), new Figure('p', 0), new Figure('p', 0), new Figure('p', 0), new Figure('p', 0), new Figure('p', 0), new Figure('p', 0), new Figure('p', 0)],
       [new Figure('v', 0), new Figure('k', 0), new Figure('s', 0), new Figure('Q', 0), new Figure('K', 0), new Figure('s', 0), new Figure('k', 0), new Figure('v', 0)]
     ];
+
+
   }
 }
--- figure.js
+++ figure.js
@@ -16,15 +16,52 @@ export default class Figure {
 
   options( arr, x, y ) {
     this.opt = [];
+    // let attack = ['p', 'v', 'k', 's', 'Q', 'K'];
     if ( this.type === 'p' ) {
       console.log(x, y);
-      if (y === 6) {
-        if ( arr[y-1][x] === null ) this.opt.push({x:x, y:y-1});
-        if ( arr[y-2][x] === null ) this.opt.push({x:x, y:y-2});
+
+      // if (y === 6) {
+      //   // if ( arr[y-1][x] === null ) this.opt.push({x:x, y:y-1});
+      //   // if ( arr[y-2][x] === null ) this.opt.push({x:x, y:y-2});
+      //   if ( arr[y-1][x] === null ) {
+      //     this.opt.push({x:x, y:y-1});
+      //     if ( arr[y-2][x] === null ) this.opt.push({x:x, y:y-2});
+      //   }
+        
+      // }
+      // else {
+      //   if ( arr[y-1][x] === null ) this.opt.push({x:x, y:y-1});
+      // }
+      
+        // if ( arr[y-1][x] === null ) this.opt.push({x:x, y:y-1});
+        // if ( arr[y-2][x] === null ) this.opt.push({x:x, y:y-2});
+
+      if ( arr[y-1][x] === null ) { // pohyb vpred
+        this.opt.push({x:x, y:y-1});
+        if ( y === 6 && arr[y-2][x] === null ) this.opt.push({x:x, y:y-2});
       }
-      else {
-        if ( arr[y-1][x] === null ) this.opt.push({x:x, y:y-1});
+
+      if (x > 1 && y > 1) {
+        // if ( arr[y-1][x-1].type === 'p' ) {
+
+        // }
+        // this.opt.push({x:x-1, y:y-1});
+        if (arr[y-1][x-1] !== null && arr[y-1][x-1].type !== 'K' ) {
+          console.log(arr[1][4].type);
+          /this.opt.push({x:x-1, y:y-1});
+          // console.log(arr[1][4].type);
+        }
+          
       }
+
+        
+     
+
+
+
+
+
+
       if (y === 1) {
         if ( arr[7-y-1][x] === null ) this.opt.push({x:7-x, y:7-y-1});
         if ( arr[7-y-2][x] === null ) this.opt.push({x:7-x, y:7-y-2});
