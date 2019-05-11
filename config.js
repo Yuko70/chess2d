b535503d370cce282b0995b2d267bcf9e211db54
Yuko70 <martin.masek10@gmail.com>
2019-05-11 15:42:00 +0200

    1 file modified

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js"></script>
+import * as firebase from 'firebase/app';
+import 'firebase/firestore'
 
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#config-web-app -->
+const firebaseConfig = {
+  apiKey: "AIzaSyA8kYRcralTqh5Tq-Tq_IBoLnZfFjt8RHg",
+  authDomain: "chessdb00.firebaseapp.com",
+  databaseURL: "https://chessdb00.firebaseio.com",
+  projectId: "chessdb00",
+  storageBucket: "chessdb00.appspot.com",
+  messagingSenderId: "475892835159",
+  appId: "1:475892835159:web:60b49026a3f07c5a"
+};
 
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyA8kYRcralTqh5Tq-Tq_IBoLnZfFjt8RHg",
-    authDomain: "chessdb00.firebaseapp.com",
-    databaseURL: "https://chessdb00.firebaseio.com",
-    projectId: "chessdb00",
-    storageBucket: "chessdb00.appspot.com",
-    messagingSenderId: "475892835159",
-    appId: "1:475892835159:web:60b49026a3f07c5a"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-</script>
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+export const database = firebase.firestore()
