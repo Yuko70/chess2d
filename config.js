6295a58ee44ca99577d5d7392c8f240398794fba
Yuko70 <martin.masek10@gmail.com>
2019-05-11 15:33:00 +0200

    1 file modified, 1 added

---
+++ config.js
@@ -0,0 +1,20 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA8kYRcralTqh5Tq-Tq_IBoLnZfFjt8RHg",
+    authDomain: "chessdb00.firebaseapp.com",
+    databaseURL: "https://chessdb00.firebaseio.com",
+    projectId: "chessdb00",
+    storageBucket: "chessdb00.appspot.com",
+    messagingSenderId: "475892835159",
+    appId: "1:475892835159:web:60b49026a3f07c5a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
