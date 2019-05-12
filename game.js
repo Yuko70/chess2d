c4aa9df31403315e4e6368f16445a80ed4abd2d8
Yuko70 <martin.masek10@gmail.com>
2019-05-12 22:50:37 +0200

    1 file modified

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 // import Canvas from './index.js'
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 // let x = document.getElementById("root");
 
 // let canvaso = new Canvas();
@@ -11,6 +11,36 @@ import React, { Component } from 'react';
 //       counter: 0
 //     }
 //   }
+
+export default class Game {
+  constructor( player ) {
+    this.playerG = player;
+
+    let btn1 = document.getElementById('potvrdTah');
+btn1.addEventListener('click', potvrdTah);
+
+let btn2 = document.getElementById('odznacTah');
+btn2.addEventListener('click', odznacTah);
+  }
+
+  potvrdTah() {
+  console.log('tah potvrdeny');
+
+  let canvas = document.getElementById("canvas");
+  let context = canvas.getContext('2d');
+  console.log('canvas',canvas);
+  console.log('context',context);
+  // console.log('canvas.player',canvas.test());
+  // console.log('context.player',context.test());
+  console.log(canvasRef);
+}
+
+
+odznacTah() {
+  console.log('tah odznaceny');
+}
+
+}
 
 function potvrdTah() {
   console.log('tah potvrdeny');
