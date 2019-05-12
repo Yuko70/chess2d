4021dfea9e661f1871a2d156b13c18583b128c3e
Yuko70 <martin.masek10@gmail.com>
2019-05-12 21:33:09 +0200

    1 file added

---
+++ game.js
@@ -0,0 +1,26 @@
+
+
+// let x = document.getElementById("root");
+
+function potvrdTah() {
+  console.log('tah potvrdeny');
+
+  let canvas = document.getElementById("canvas").getContext('2d');
+  console.log(canvas.player);
+}
+
+function odznacTah() {
+  console.log('tah odznaceny');
+}
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+let btn1 = document.getElementById('potvrdTah');
+btn1.addEventListener('click', potvrdTah);
+
+let btn2 = document.getElementById('odznacTah');
+btn2.addEventListener('click', odznacTah);
